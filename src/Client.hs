78dee81ddf67cd7e8b6151aceeb3238df9b8d3b1
Anurag Ohri <anuragohri92@gmail.com>
2018-04-03 11:40:36 +0530

    Just some tests with Client.hs

--- src/Client.hs
+++ src/Client.hs
@@ -5,15 +5,22 @@
 
 import Turtle
 import qualified Data.Text as T
+import Data.Maybe(mapMaybe)
 
 host = "dmrc@172.22.107.1"
 buildDirectory = "~/ASPMS/.stack-work/install/x86_64-linux/lts-8.5/8.0.2/bin"
 buildFile = "ASPMS"
 
-main = do
-    -- Trigger Build
-    shell (T.unwords ["ssh", host, "./ASPMS/Build.hs"]) empty
-    
-    -- Fetch Binary
-    shell (T.unwords ["scp", "~", mconcat [host, ":", buildDirectory, buildFile]]) empty
-    putStrLn "Done"
+commands = -- mapMaybe textToLine ["ls -l"]
+    map unsafeTextToLine ["echo Hello World!", "cd ASPMS ", "pwd ", "ls . "]
+
+main =
+    -- -- Trigger Build
+    -- shell (T.unwords ["ssh", host, "chmod 755 ~/ASPMS/Build.hs"]) empty .&&.
+    --     shell (T.unwords ["ssh", host, "cd ASPMS"]) empty .&&.
+    --     shell (T.unwords ["ssh", host, "./Build.hs"]) empty .&&.
+        
+    --     -- Fetch Binary
+    --     shell (T.unwords ["scp", "~/", mconcat [host, ":", buildDirectory, buildFile]]) empty
+    -- putStrLn "Done"
+    shell (T.unwords ["ssh", host]) $ select commands
